120a2e19706e11198bd3c00fa348d5a686b41912
root <root@precise64.(none)>
2014-01-25 15:21:18 +0000

    added mysql cookbook

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,10 +1,9 @@
 log_level                :info
 log_location             STDOUT
-node_name                'ahmad'
-client_key               '/home/ahmad/chef-client/chef-repo/.chef/ahmad.pem'
+node_name                'vagrant'
+client_key               '/home/vagrant/my_repo/chef-repo/.chef/vagrant.pem'
 validation_client_name   'chef-validator'
-validation_key           '/home/ahmad/chef-client/chef-repo/.chef/chef-validator.pem'
-chef_server_url          'https://192.168.56.102'
-syntax_check_cache_path  '/home/ahmad/chef-client/chef-repo/.chef/syntax_check_cache'
-cookbook_path [ '/home/ahmad/chef-client/chef-repo/cookbooks' ]
-knife[:editor] ="vim"
+validation_key           '/home/vagrant/my_repo/chef-repo/.chef/chef-validator.pem'
+chef_server_url          'https://192.168.50.11'
+syntax_check_cache_path  '/home/vagrant/my_repo/chef-repo/.chef/syntax_check_cache'
+cookbook_path [ '/home/vagrant/my_repo/chef-repo/cookbooks' ]
